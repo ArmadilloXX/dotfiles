f932ed6fda84994aedd2710fff9258daacaaca85
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-08-01 17:43:19 +0300

    Add install

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CURRENT_DIR=`pwd`
+
+ln -sf $CURRENT_DIR/git_template ~/.git_template
+
+# ln -sf $CURRENT_DIR/vim ~/.vim
+# ln -sf $CURRENT_DIR/vim/vimrc ~/.vimrc
+# ln -sf $CURRENT_DIR/vim/gvimrc ~/.gvimrc
+
+# curl -L http://install.ohmyz.sh | sh
+# cp -sf $CURRENT_DIR/zsh/themes/excess.zsh-theme ~/.oh-my-zsh/themes/excess.zsh-theme
+# ln -sf $CURRENT_DIR/zsh/zshrc ~/.zshrc
+
+# ln -sf $CURRENT_DIR/ctags ~/.ctags
+# ln -sf $CURRENT_DIR/tmux ~/.tmux.conf
+
+# ln -sf $CURRENT_DIR/ruby/rubocop.yml ~/.rubocop.yml
+# ln -sf $CURRENT_DIR/ruby/gemrc ~/.gemrc
+# ln -sf $CURRENT_DIR/ruby/irbrc ~/.irbrc
+
+# cp -sf $CURRENT_DIR/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+
+ln -sf $CURRENT_DIR/git/gitconfig ~/.gitconfig
+# ln -sf $CURRENT_DIR/git/gitignore_global ~/.gitignore_global
+# ln -sf $CURRENT_DIR/git/hooks/post-commit ~/.git/hooks/post-commit
